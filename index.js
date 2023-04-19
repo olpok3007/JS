f10f378d019a3c9b29cb6b921fb67160816dd8a1
Oleg Pokotylo <olegpokotylo@gmail.com>
2023-04-19 22:45:58 +0200

    add

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
 "use strict";
-function User(name, age, isMale) {
-  this.name = name;
+function User(firstName, lastName, email, age, isMale, isSubscribed) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.email = email;
   this.age = age;
   this.isMale = isMale;
-
-  this.speak = function (works) {
-    return `${this.name} says: ${works}`;
-  };
+  this.isSubscribed = isSubscribed;
 }
 
-const user1 = new User("First Name", 50);
+// const user1 = new User("Ivan", "Ivanowycz", "ivan@gmail.com", 25, true, false);
+
+function createusers(amount) {
+  const userArray = [];
+  for (let i = 0; i < amount; i++) {
+    const user = new User(
+      `Name ${i}`,
+      `LastName ${i}`,
+      `email${i}@email.com`,
+      Math.round(Math.random() * 100),
+      Math.random() > 0.5,
+      Math.random() > 0.5
+    );
+    userArray.push(user);
+  }
+
+  return userArray;
+}
+
+const userArray1 = createusers(100);
+console.table(userArray1);
+
+console.table(
+  userArray1.map(function (rrr) {
+    return rrr.email;
+  })
+);
+console.table(
+  userArray1.filter(function (fff) {
+    return fff.age > 95;
+  })
+);
+
+console.table(
+  userArray1.filter(function (isFilterMan) {
+    return isFilterMan.age >= 50 && isFilterMan;
+  })
+);
+// function User(name, age, isMale) {
+//   this.name = name;
+//   this.age = age;
+//   this.isMale = isMale;
+// }
 
 // const user0 = {
 //   name: "Test",
